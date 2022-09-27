618f48fa6cb4a315e57fe5d7ab0e10bb209962df
Bell <hayleeb13@gmail.com>
2022-09-27 03:04:34 -0700

    Assignment four changes

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -5,7 +5,11 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -44,7 +48,16 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
 		
-		//TODO  complete this method in homework 4
+		System.out.println("Receive enrollment :" + enrollmentDTO);
+		Enrollment enrollment = new Enrollment();
+		enrollment.setStudentEmail(enrollmentDTO.studentEmail);
+		enrollment.setStudentName(enrollmentDTO.studentName);
+		Course course = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if(course==null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found");
+		}
+		enrollment.setCourse(course);
+		enrollment = enrollmentRepository.save(enrollment);
 		
 	}
 
@@ -52,7 +65,7 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@Override
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
 		 
-		//TODO  complete this method in homework 4
+		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
 		
 	}
 
